10b8ba71f3831fc0d1fc2dc3b7834460d76e9350
REANI_VL <satenmisaka10032@gmail.com>
2021-06-17 11:26:22 +0900

    吉川用

--- DirectX12_20210606/DirectX12/DirectX12 Game Solution/DirectX12 MainProject/MainScene.cpp
+++ DirectX12_20210606/DirectX12/DirectX12 Game Solution/DirectX12 MainProject/MainScene.cpp
@@ -21,7 +21,31 @@ void MainScene::Initialize()
 // Allocate all memory the Direct3D and Direct2D resources.
 void MainScene::LoadAssets()
 {
+    descriptorHeap = DX12::CreateDescriptorHeap(DXTK->Device, 1);
 
+    ResourceUploadBatch resourceUploadBatch(DXTK->Device);
+    resourceUploadBatch.Begin();
+
+    RenderTargetState rtState(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_FORMAT_D32_FLOAT);
+    SpriteBatchPipelineStateDescription pd(rtState, &CommonStates::NonPremultiplied);
+    D3D12_VIEWPORT viewport = {
+        0.0f, 0.0f, 1280.0f, 720.0f,
+        D3D12_MIN_DEPTH, D3D12_MAX_DEPTH
+    };
+
+    spriteBatch = DX12::CreateSpriteBatch(DXTK->Device, resourceUploadBatch, pd, &viewport);
+
+    dx9GpuDescriptor = DXTK->Direct3D9->CreateShaderResourceView(descriptorHeap.get(), 0);
+
+    auto uploadResourcesFinished = resourceUploadBatch.End(DXTK->CommandQueue);
+    uploadResourcesFinished.wait();
+
+
+    // グラフィックリソースの初期化処理
+
+    //プレイヤー
+    player_sprite_ = DX9::Sprite::CreateFromFile(DXTK->Device9, L"robot_e.png");
+    bg_sprite_ = DX9::Sprite::CreateFromFile(DXTK->Device9, L"bg.png");
 }
 
 // Releasing resources required for termination.
@@ -49,11 +73,21 @@ void MainScene::OnRestartSound()
 // Updates the scene.
 NextScene MainScene::Update(const float deltaTime)
 {
+    constexpr float  SPEED = 500.0f;
+    const float SQUARE_X = DXTK->GamePadState[0].thumbSticks.leftX;
+    const float SQUARE_Y = DXTK->GamePadState[0].thumbSticks.leftY;
+
+    SimpleMath::Vector3 movement;
+    movement.x = SQUARE_X * sqrt(1.0f - 0.5f * SQUARE_Y * SQUARE_Y);
+    movement.y = SQUARE_Y * sqrt(1.0f - 0.5f * SQUARE_X * SQUARE_X);
+   
+    player_position_ += movement * SPEED * deltaTime;
+
 	// If you use 'deltaTime', remove it.
 	UNREFERENCED_PARAMETER(deltaTime);
 
 	// TODO: Add your game logic here.
-
+    player_X = std::clamp(player_X, PLAYER_WIDTH1, PLAYER_WIDTH2 - PLAYER_WIDTH1);
 
 
 	return NextScene::Continue;
@@ -63,10 +97,75 @@ NextScene MainScene::Update(const float deltaTime)
 void MainScene::Render()
 {
 	// TODO: Add your rendering code here.
-	DXTK->ResetCommand();
+    DXTK->Direct3D9->Clear(DX9::Colors::RGBA(0, 0, 0, 255));
+
+    DXTK->Direct3D9->BeginScene();
+    DX9::SpriteBatch->Begin();
+
+    DX9::SpriteBatch->DrawSimple(player_sprite_.Get(), SimpleMath::Vector3(player_X, player_Y, 0.0f));
+    DX9::SpriteBatch->DrawSimple(bg_sprite_.Get(), SimpleMath::Vector3(0.0, 0.0f, 4.0f));
+
+
+
+
+    DX9::SpriteBatch->End();
+    DXTK->Direct3D9->EndScene();
+
+    DXTK->Direct3D9->UpdateResource();
+
+    DXTK->ResetCommand();
 	DXTK->ClearRenderTarget(Colors::CornflowerBlue);
 
+    const auto heapes = descriptorHeap->Heap();
+    DXTK->CommandList->SetDescriptorHeaps(1, &heapes);
 
+    spriteBatch->Begin(DXTK->CommandList);
+    spriteBatch->Draw(
+        dx9GpuDescriptor,
+        XMUINT2(1280, 720),
+        SimpleMath::Vector2(0.0f, 0.0f)
+    );
+    spriteBatch->End();
+
+    DXTK->Direct3D9->WaitUpdate();
 
 	DXTK->ExecuteCommandList();
+}
+
+void MainScene::PlayerController(const float deltaTime)
+{
+
+    //プレイヤー（キーボード）
+    /*if (DXTK->KeyState->Right) {
+        player_X += player_move_speed * deltaTime;
+    }
+
+    if (DXTK->KeyState->Left) {
+        player_X += -player_move_speed * deltaTime;
+    }
+
+    if (DXTK->KeyState->Up) {
+        player_Y += -player_move_speed * deltaTime;
+    }
+
+    if (DXTK->KeyState->Down) {
+        player_Y += player_move_speed * deltaTime;
+    }*/
+
+    //プレイヤーの移動範囲
+    if (player_X < player_minimum_said) {
+        player_X = player_minimum_said;
+    }
+
+    if (player_X > player_full_said) {
+        player_X = player_full_said;
+    }
+
+    if (player_Y < player_minimum_vertical) {
+        player_Y = player_minimum_vertical;
+    }
+
+    if (player_Y > player_hull_vertical) {
+        player_Y = player_hull_vertical;
+    }
 }
--- DirectX12_20210606/DirectX12/DirectX12 Game Solution/DirectX12 MainProject/MainScene.h
+++ DirectX12_20210606/DirectX12/DirectX12 Game Solution/DirectX12 MainProject/MainScene.h
@@ -35,7 +35,37 @@ public:
 	void Render() override;
 
 private:
+	DX12::DESCRIPTORHEAP descriptorHeap;
+	DX12::SPRITEBATCH    spriteBatch;
+	DX12::HGPUDESCRIPTOR dx9GpuDescriptor;
 
+	// ïœêîÇÃêÈåæ
+	void PlayerController(const float deltaTime);
 
+	DX9::SPRITE player_sprite_;
+	DX9::SPRITE bg_sprite_;
+
+	SimpleMath::Vector3 player_position_;
+
+	const float PLAYER_WIDTH1 = -180.0f;
+	const float PLAYER_WIDTH2 = 1280.0f;
+
+	float player_X = 0.0f;
+	float player_Y = 360.0f;
+
+	float player_move_speed = 900.0f;
+
+	float player_minimum_said = 0 - 20;
+	float player_full_said = 1280 - 100;
+
+	float player_minimum_vertical = 0 - 20;
+	float player_hull_vertical = 720 - 150;
+
+	float xCircle;
+	float yCircle;
+	float XSquare;
+	float YSquare;
+	float xSquare;
+	float ySquare;
 
 };
